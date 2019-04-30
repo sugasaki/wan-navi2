ada5f66b366b9067ae983d784788470c10fb9ee6
sugasaki <sugasaki@gmail.com>
2019-04-30 11:40:23 +0900

    _fetchで全てのユーザーデータを取得していたので変更した

--- wan-navi2/index.js
+++ wan-navi2/index.js
@@ -1,51 +1,47 @@
-
-
 // Setup leaflet map
-var map = new L.Map('map');
-var basemapLayer = new L.TileLayer('http://{s}.tiles.mapbox.com/v3/github.map-xgq2svrz/{z}/{x}/{y}.png');
+var map = new L.Map("map");
+var basemapLayer = new L.TileLayer(
+  "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'"
+);
 // Adds the background layer to the map
 map.addLayer(basemapLayer);
 
-
-var runLayer = omnivore.kml('./mapdata/217.kml')
-.on('ready', function () {
+var runLayer = omnivore
+  .kml("./mapdata/217.kml")
+  .on("ready", function() {
     // map.fitBounds(runLayer.getBounds());
-})
-.addTo(map);
+  })
+  .addTo(map);
 
 map.setView([32.760886, 129.865541], 13);
-
 
 //スケールコントロールを追加（オプションはフィート単位を非表示）
 //L.control.scale({imperial: false}).addTo(map);
 
-
 var moveIcon = L.icon({
-    //iconUrl: 'images/sugapon3.png',
-    //shadowUrl: 'images/marker-shadow.png',
+  //iconUrl: 'images/sugapon3.png',
+  //shadowUrl: 'images/marker-shadow.png',
 
-    iconSize: [25, 41],
-    shadowSize: [41, 41],
-    iconAnchor: [13, 40],
-    shadowAnchor: [12, 40],
-    popupAnchor: [0, -32]
+  iconSize: [25, 41],
+  shadowSize: [41, 41],
+  iconAnchor: [13, 40],
+  shadowAnchor: [12, 40],
+  popupAnchor: [0, -32]
 });
-
 
 // Playback options
 var playbackOptions = {
-    playControl: true,
-    dateControl: false,
-    sliderControl: false ,   
-    speed: 10,
-    tracksLayer: true,
-    marker: function(){
-        return {
-            //icon:  moveIcon 
-        };
-    }      
+  playControl: true,
+  dateControl: false,
+  sliderControl: false,
+  speed: 10,
+  tracksLayer: true,
+  marker: function() {
+    return {
+      //icon:  moveIcon
+    };
+  }
 };
-
 
 var timelineData;
 var timelineOptions = {};
@@ -53,13 +49,11 @@ var timeline;
 var playback;
 var lastUpdateTIme;
 
-
-
-var id = setInterval(
-    function(){
-        _refresh();　//idをclearIntervalで指定している
-    }, 30000);
-
+/*
+var id = setInterval(function() {
+  _refresh(); //idをclearIntervalで指定している
+}, 30000);
+*/
 
 /*
 var id = setTimeout(
@@ -68,143 +62,146 @@ var id = setTimeout(
         $('input[class="leaflet-control-layers-selector"]').prop("checked", true);
     }, 5000);
 */
-    
 
+var id = setTimeout(function() {
+  _fetch(); //idをclearIntervalで指定している
+}, 100);
 
-_fetch();
-
-
-
-function _refresh(){
-
-    var unix_timespamp = lastUpdateTIme.format("x");  
-    var url = 'https://wan-navi.azurewebsites.net/api/LeafletPlayback/' + unix_timespamp;
-    fetch(url, {
-        method: "GET", 
-    })
-    .then((response) => response.json())
-    .then((responseJson) => {
-        refreshTimeLine(responseJson);
-        lastUpdateTIme = moment();
+function _refresh() {
+  var userid = location.hash;
+  var unix_timespamp = lastUpdateTIme.format("x");
+  var url =
+    "https://wan-navi.azurewebsites.net/api/LeafletPlayback/" + unix_timespamp;
+  fetch(url, {
+    method: "GET"
+  })
+    .then(response => response.json())
+    .then(responseJson => {
+      refreshTimeLine(responseJson);
+      lastUpdateTIme = moment();
     });
+}
 
-};
+function _fetch() {
+  var userid = location.hash;
+  console.log("userid", userid);
+  if (!userid) return;
 
+  userid = userid.substr(1); //"3456"
+  $("#uid").html(userid);
 
-function _fetch(){
+  var url = "https://wan-navi.azurewebsites.net/api/LeafletPlayback/" + userid;
+  //var url = 'http://localhost:5000/api/LeafletPlayback/' + userid;
 
-    var userid = location.hash;
-    userid = userid.substr(1); //"3456"
-    $('#uid').html( userid );
+  console.log("url", url);
 
-    var url = 'https://wan-navi.azurewebsites.net/api/LeafletPlayback/' + userid;
-    //var url = 'http://localhost:5000/api/LeafletPlayback/' + userid;
-    
-
-    fetch(url, {
-        method: "GET", 
-    })
-    .then((response) => response.json())
-    .then((responseJson) => {
-        inittimelinn(responseJson);
-        // Initialize playback
-        playback = new L.Playback(map, responseJson, onPlaybackTimeChange, playbackOptions);
-        playback._map.fitBounds(playback._tracksLayer.layer.getBounds());
-        refreshNow(responseJson);
+  fetch(url, {
+    method: "GET"
+  })
+    .then(response => response.json())
+    .then(responseJson => {
+      console.log("responseJson", responseJson);
+      inittimelinn(responseJson);
+      // Initialize playback
+      playback = new L.Playback(
+        map,
+        responseJson,
+        onPlaybackTimeChange,
+        playbackOptions
+      );
+      playback._map.fitBounds(playback._tracksLayer.layer.getBounds());
+      refreshNow(responseJson);
     });
-};
-
-
+}
 
 var startTime;
 var endTime;
 
-
-
-function refreshNow(demoTracks){
-    endTime = new Date(demoTracks.properties.time[demoTracks.properties.time.length - 1]);
-    var ms = endTime.valueOf(); 
-    playback.setCursor(ms);
+function refreshNow(demoTracks) {
+  endTime = new Date(
+    demoTracks.properties.time[demoTracks.properties.time.length - 1]
+  );
+  var ms = endTime.valueOf();
+  playback.setCursor(ms);
 }
 
+function refreshTimeLine(demoTracks) {
+  if (demoTracks.geometry.coordinates.length == 0) return;
 
+  endTime = new Date(
+    demoTracks.properties.time[demoTracks.properties.time.length - 1]
+  );
+  playback.addData(demoTracks);
 
-function refreshTimeLine(demoTracks){
-    if (demoTracks.geometry.coordinates.length == 0) return;
+  var timelineEnd = new Date(endTime);
+  var item1 = timelineData.get(1);
+  item1.end = timelineEnd;
+  timelineData.update(item1);
 
-    endTime = new Date(demoTracks.properties.time[demoTracks.properties.time.length - 1]);
-    playback.addData(demoTracks);
+  timeline.setWindow(startTime, timelineEnd);
+  timeline.redraw();
 
-    var timelineEnd = new Date(endTime);
-    var item1 = timelineData.get(1);
-    item1.end = timelineEnd;
-    timelineData.update(item1);
+  // Set custom time marker (blue)
+  timeline.setCurrentTime(endTime);
+  timeline.setCustomTime(endTime);
+  playback.setCursor(endTime.valueOf());
 
-    timeline.setWindow(startTime, timelineEnd);
-    timeline.redraw();
-
-
-    // Set custom time marker (blue)
-    timeline.setCurrentTime(endTime);
-    timeline.setCustomTime(endTime);
-    playback.setCursor(endTime.valueOf());
-
-    var ms = lastUpdateTIme.valueOf(); 
-    onPlaybackTimeChange(ms);
+  var ms = lastUpdateTIme.valueOf();
+  onPlaybackTimeChange(ms);
 }
 
+function inittimelinn(demoTracks) {
+  var a = $(".leaflet-control-layers-selector");
 
+  lastUpdateTIme = moment();
 
-function inittimelinn(demoTracks){
+  // Get start/end times
+  startTime = new Date(demoTracks.properties.time[0]);
+  endTime = new Date(
+    demoTracks.properties.time[demoTracks.properties.time.length - 1]
+  );
 
-    var a = $(".leaflet-control-layers-selector");
+  // Create a DataSet with data
+  timelineData = new vis.DataSet([
+    { id: 1, start: startTime, end: endTime, content: "" }
+  ]);
 
+  // Set timeline options
+  timelineOptions = {
+    width: "100%",
+    height: "120px",
+    style: "box",
+    axisOnTop: true,
+    showCustomTime: true,
+    autoResize: true
+  };
 
-    lastUpdateTIme = moment();
+  // Setup timeline
+  timeline = new vis.Timeline(
+    document.getElementById("timeline"),
+    timelineData,
+    timelineOptions
+  );
 
-    // Get start/end times
-    startTime = new Date(demoTracks.properties.time[0]);
-    endTime = new Date(demoTracks.properties.time[demoTracks.properties.time.length - 1]);
+  // Set custom time marker (blue)
+  //timeline.setCustomTime(startTime);
+  //timeline.setCurrentTime(startTime);
 
-    // Create a DataSet with data
-    timelineData = new vis.DataSet([{ id: 1, start: startTime, end: endTime, content: '' }]);
-
-    // Set timeline options
-    timelineOptions = {
-        "width":  "100%",
-        "height": "120px",
-        "style": "box",
-        "axisOnTop": true,
-        "showCustomTime": true,
-        "autoResize": true, 
-    };
-    
-    // Setup timeline
-    timeline = new vis.Timeline(document.getElementById('timeline'), timelineData, timelineOptions);
-        
-    // Set custom time marker (blue)
-    //timeline.setCustomTime(startTime);
-    //timeline.setCurrentTime(startTime);
-
-    // Set timeline time change event, so cursor is set after moving custom time (blue)
-    timeline.on('timechange', onCustomTimeChange);    
-
+  // Set timeline time change event, so cursor is set after moving custom time (blue)
+  timeline.on("timechange", onCustomTimeChange);
 }
-
-
-
 
 // A callback so timeline is set after changing playback time
-function onPlaybackTimeChange (ms) {
-    timeline.setCustomTime(new Date(ms));
-    var day = moment(ms);
-    var st = day.format('YYYY/MM/DD HH:mm:ss ');
-    $('#datetime').html( st );
-};
+function onPlaybackTimeChange(ms) {
+  timeline.setCustomTime(new Date(ms));
+  var day = moment(ms);
+  var st = day.format("YYYY/MM/DD HH:mm:ss ");
+  $("#datetime").html(st);
+}
 
-// 
+//
 function onCustomTimeChange(properties) {
-    if (!playback.isPlaying()) {
-        playback.setCursor(properties.time.getTime());
-    }        
-}       
+  if (!playback.isPlaying()) {
+    playback.setCursor(properties.time.getTime());
+  }
+}
